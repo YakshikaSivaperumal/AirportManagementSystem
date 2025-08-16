30147e7557aed26a89ce1488c8915ad29b9a8083
Yakshika Sivaperumal <132678922+YakshikaSivaperumal@users.noreply.github.com>
2025-08-16 21:51:49 +0530

    Last commit

---
+++ airport-management-system-main/src/coe528lab1/Member.java
@@ -0,0 +1,23 @@
+package coe528lab1;
+public class Member extends Passenger{
+    
+    int yearsOfMembership;
+
+    public Member(String name, int age){
+        super(name, age);
+    }
+
+    public double applyDiscount(double p){
+        if (yearsOfMembership > 5){
+            System.out.println("Applying 10% discount...");
+            return p - (p*(0.1));
+        }
+        else if(yearsOfMembership > 1){
+            System.out.println("Applying 50% discount...");
+            return p - (p*(0.5));
+        }
+        else
+        return p;
+    }
+
+}
